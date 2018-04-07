febd04f343b1b9ad92df19515d02d867c436db35
igorand2018 <qa0208@yandex.ru>
2018-04-07 22:14:32 +0300

    changed color of figures in canvas

--- canvas.js
+++ canvas.js
@@ -5,6 +5,11 @@ var ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'blue';
 ctx.fillRect(0, 0, 200, 150);
+ctx.fillStyle = 'red';
 ctx.fillRect(210, 0, 30, 30);
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'blue';
+ctx.strokeRect(210, 40, 30, 30);
 ctx.fillRect(210, 40, 30, 30);
+ctx.fillStyle = 'black';
 ctx.fillRect(250, 0, 200, 150);
